17684153a74d78150d254e8832ef361f5c715b83
DevsNetDesign <developers@netdesign-ec.net>
2025-03-03 02:13:46 -0500

    instalacion de package

---
+++ Fantasy.BackEnd/Data/DataContex.cs
@@ -0,0 +1,20 @@
+﻿using Fantasy.Shared.Entites.GeneralParameters;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.BackEnd.Data
+{
+    public class DataContex : DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Fantasy.Shared/Entites/GeneralParameters/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fantasy.Shared.Entites.GeneralParameters
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El maximo número de caracteres es {0}")]
+        [Required]
+        public string? Name { get; set; }
+    }
+}
